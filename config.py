6e2a7a8e22747cd4861335b52b7acc8e170ade08
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-07-15 19:06:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(0123456789)
+ADMINS.append(1234567890)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
