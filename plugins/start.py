ab533f7aaaabde8ff3f80c57bf773277c74c2b16
Shivdwivediji <115983642+Shivdwivediji@users.noreply.github.com>
2023-08-05 10:24:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -82,8 +82,8 @@ async def start_command(client: Client, message: Message):
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-        await asyncio.sleep(60)
-        msg.delete_message(chat_id=msg.chat_id, message_id=msg.message_id)
+        await asyncio.sleep(10)
+        msg.delete(chat_id=msg.chat_id, message_id=msg.message_id)
         return
     else:
         reply_markup = InlineKeyboardMarkup(
