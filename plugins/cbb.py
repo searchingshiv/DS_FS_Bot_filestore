29d96a72669f74b3269f3a3f420580d61742ac6e
Shivdwivediji <115983642+Shivdwivediji@users.noreply.github.com>
2023-08-05 11:04:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> ○ Owner : <a href='https://t.me/Unknownbotking'>King Bot </a>\n ○ Channel : <a href='https://t.me/NAVANEETHANDS'>Dot Studio </a>\n ○ Developer : <a href='https://t.me/searchingshiv'>Unknown </a></b>",
+            text = f"<b>PERVERTS</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
